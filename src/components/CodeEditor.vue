b0950c2a4b794c9a58a141d2ca3b4e9a5c68d8bf
Ethan <neh9483@uncw.edu>
2026-03-31 00:10:25 -0400

    testing code mirror

--- src/components/CodeEditor.vue
+++ src/components/CodeEditor.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
-import { handleError, onMounted, ref } from 'vue';
-import { CodeEditor, useCodeEditor, type EditorOptions } from 'monaco-editor-vue3';
+import { handleError, onMounted, ref, shallowRef } from 'vue';
+// import { CodeEditor, useCodeEditor, type EditorOptions } from 'monaco-editor-vue3';
 import { runUserCode, startCode } from '@/assets/api';
+import { Codemirror } from 'vue-codemirror';
+import { javascript } from '@codemirror/lang-javascript'
+import { oneDark } from '@codemirror/theme-one-dark'
 
 import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
 import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
@@ -9,40 +12,47 @@ import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
 import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
 import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
 
-const code = ref(startCode)
+const code = ref<string | null>(null)
+const extensions = [javascript(), oneDark]
 
-const editorOptions: EditorOptions = {
-  fontSize: 14,
-  minimap: { enabled: false },
-  automaticLayout: true
+// Codemirror EditorView instance ref
+const view = shallowRef(null)
+const handleReady = (payload) => {
+    view.value = payload.view
 }
 
-function handleMount(editor: any) {
+// const editorOptions: EditorOptions = {
+//   fontSize: 14,
+//   minimap: { enabled: false },
+//   automaticLayout: true
+// }
+
+// function handleMount(editor: any) {
   
-}
+// }
 
-function handleErr(editor: any) {
-  console.log(editor)
-}
+// function handleErr(editor: any) {
+//   console.log(editor)
+// }
 
 onMounted(() => {
-  self.MonacoEnvironment = {
-    getWorker(_, label) {
-      if (label === 'json') {
-        return new jsonWorker()
-      }
-      if (label === 'css' || label === 'scss' || label === 'less') {
-        return new cssWorker()
-      }
-      if (label === 'html' || label === 'handlebars' || label === 'razor') {
-        return new htmlWorker()
-      }
-      if (label === 'typescript' || label === 'javascript') {
-        return new tsWorker()
-      }
-      return new editorWorker()
-    }
-  }
+//   self.MonacoEnvironment = {
+//     getWorker(_, label) {
+//       if (label === 'json') {
+//         return new jsonWorker()
+//       }
+//       if (label === 'css' || label === 'scss' || label === 'less') {
+//         return new cssWorker()
+//       }
+//       if (label === 'html' || label === 'handlebars' || label === 'razor') {
+//         return new htmlWorker()
+//       }
+//       if (label === 'typescript' || label === 'javascript') {
+//         return new tsWorker()
+//       }
+//       return new editorWorker()
+//     }
+//   }
 
   // monaco.editor.create(document.getElementById('container'), {
   //   value: "function hello() {\n\talert('Hello world!');\n}",
@@ -53,13 +63,26 @@ onMounted(() => {
 
 <template>
   <div class="editor">
-    <CodeEditor
+    <!-- <CodeEditor
       v-model:value="code"
       language="javascript"
       theme="vs-dark"
       :options="editorOptions"
       @mount="handleMount"
       @error="handleErr"
+    /> -->
+    <codemirror
+        v-model="code"
+        placeholder="Code goes here..."
+        :style="{ overflow: 'auto' }"
+        :autofocus="true"
+        :indent-with-tab="true"
+        :tab-size="4"
+        :extensions="extensions"
+        @ready="handleReady"
+        @change="console.log('change', $event)"
+        @focus="console.log('focus', $event)"
+        @blur="console.log('blur', $event)"
     />
     <button @click="runUserCode(code)" class="run-button">Run</button>
   </div>
@@ -67,7 +90,7 @@ onMounted(() => {
 
 <style scoped>
 .editor {
-    overflow-y: auto;
+    overflow-y: scroll;
 }
 .run-button {
     position: absolute;
