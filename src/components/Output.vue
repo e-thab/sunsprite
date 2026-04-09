fe8e51d5b6ebeb8afa7ee1e13fc803cc8f3e527d
Ethan <playfiend@gmail.com>
2026-04-09 13:32:19 -0400

    output wip, watch tab

--- src/components/Output.vue
+++ src/components/Output.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 
 // const panel = ref<HTMLElement>()
 
@@ -18,21 +18,53 @@ import { onMounted, ref } from 'vue';
 //     }
 // })
 
+type OutputTab = 'output' | 'watch'
+const activeTab = ref<OutputTab>('output')
+const outputTabColor = ref(getTabColor('output'))
+const watchTabColor = ref(getTabColor('watch'))
+
+const outputActive = ref(true)
+const watchActive = ref(false)
+
+function getTabColor(tab: OutputTab) {
+    return tab === activeTab.value ? '#2d3341' : '#252a33'
+}
+
+function setActiveTabColors() {
+    outputTabColor.value = getTabColor('output')
+    watchTabColor.value = getTabColor('watch')
+}
+
+function activateOutputTab() {
+    activeTab.value = 'output'
+    setActiveTabColors()
+    outputActive.value = true
+    watchActive.value = false
+}
+
+function activateWatchTab() {
+    activeTab.value = 'watch'
+    setActiveTabColors()
+    outputActive.value = false
+    watchActive.value = true
+}
+
 </script>
 
 
 <template>
     <div class="output-wrapper">
         <div class="output-header">
-            <div>Output</div>
+            <div @click="activateOutputTab" class="output-header-item output-tab">Output</div>
+            <div @click="activateWatchTab" class="output-header-item watch-tab">Watch</div>
         </div>
         <!-- <hr style="border-color: #252525; border-style:solid"> -->
-        <div class="output-panel" id="output-panel" ref="panel">
-            <!-- <div class="output-item">Item 1</div>
-            <div class="output-item">Item 2</div>
-            <div class="output-item">Item 3</div>
-            <div class="output-item">Item 4</div>
-            <div class="output-item">Item 5</div> -->
+        <div v-show="outputActive" class="output-panel" id="output-panel" ref="panel">
+            <!-- Output items are inserted here -->
+        </div>
+
+        <div v-show="watchActive" class="watch-panel">
+            Watch
         </div>
     </div>
 </template>
@@ -52,6 +84,19 @@ import { onMounted, ref } from 'vue';
     justify-content: center;
     color: #d8dee9;
     height: 24px;
+}
+
+.output-header-item {
+    flex-grow: 1;
+    text-align: center;
+}
+
+.output-tab {
+    background-color: v-bind(outputTabColor);
+}
+
+.watch-tab {
+    background-color: v-bind(watchTabColor);
 }
 
 .output-panel {
@@ -77,5 +122,11 @@ import { onMounted, ref } from 'vue';
     color: #4A546A;
     border-right: 1px solid;
     padding: 0 .25em;
+}
+
+.watch-panel {
+    display: flex;
+    justify-content: center;
+    align-content: center;
 }
 </style>
