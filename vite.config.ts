35e3b825fb61aba2f2438b544fe26282d3c72344
Ethan <playfiend@gmail.com>
2026-06-01 02:44:00 -0400

    vite base update

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  base: '/sunsprite/'
   // optimizeDeps: {
   //   include: [
   //     `${prefix}/language/json/json.worker`,
