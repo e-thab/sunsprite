bbd94315911642f112f20f21df05decb9699780c
Ethan <playfiend@gmail.com>
2026-08-10 13:07:10 -0400

    add console to monaco lib

--- src/assets/api/apiLib.ts
+++ src/assets/api/apiLib.ts
@@ -433,7 +433,28 @@ declare function play(): void
 /**
  * Clear all messages from the output panel.
  */
-declare function clearOutput(): void`,
+declare function clearOutput(): void
+
+/**
+ * The browser's dev tools console. Not part of the Sunsprite API, use print/warn/err instead to show
+ * messages in the output panel.
+ */
+declare const console: {
+    log(...data: any[]): void
+    info(...data: any[]): void
+    debug(...data: any[]): void
+    warn(...data: any[]): void
+    error(...data: any[]): void
+    trace(...data: any[]): void
+    table(...data: any[]): void
+    group(...data: any[]): void
+    groupEnd(): void
+    time(label?: string): void
+    timeEnd(label?: string): void
+    count(label?: string): void
+    assert(condition?: boolean, ...data: any[]): void
+    clear(): void
+}`,
 
 // Utilities
 `
